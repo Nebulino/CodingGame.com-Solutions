4ed2ed1e2184495dfdf8c26eb482308b863c62dd
Nebulino <l.mcclermont@gmail.com>
2017-03-27 14:14:00 +0200

    Create @-|4|-@.py

---
+++ @-|4|-@.py
@@ -0,0 +1,14 @@
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+tot = 0
+n = int(input())
+for i in range(n):
+    test = input()
+    for j in test:
+        if j == '|':
+            tot += 1
+print(int(tot/2))
