2239226ac8f80d174b71a204d5665e7f7e1835a8
Nebulino <l.mcclermont@gmail.com>
2017-03-27 16:18:10 +0200

    Create Reverse - sum a column of numbers.py

---
+++ Reverse - sum a column of numbers.py
@@ -0,0 +1,11 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+sum = 0
+n = int(input())
+for i in range(n):
+    x = int(input())
+    sum += x
+print(sum)
