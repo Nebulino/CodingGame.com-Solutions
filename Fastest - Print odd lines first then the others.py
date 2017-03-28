e993ec269725ffecbecb446a280aac4d167576b6
Nebulino <l.mcclermont@gmail.com>
2017-03-28 18:00:38 +0200

    Update Fastest - Print odd lines first then the others.py

--- Fastest - Print odd lines first then the others.py
+++ Fastest - Print odd lines first then the others.py
@@ -15,3 +15,24 @@ for c in l:
     print(c)
 for x in m:
     print(x)
+
+    
+    or...
+    
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+t=[]
+for i in range(n):
+    line = input()
+    if not i%2:print(line)
+    else:t+=[line]
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+print("\n".join(t))
