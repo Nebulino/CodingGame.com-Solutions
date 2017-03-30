ed9e7771560afd0b72c145e1fc2951032cff3110
Nebulino <l.mcclermont@gmail.com>
2017-03-30 22:26:20 +0200

    Create A+B - A+B.py

---
+++ Reverse/A+B - A+B.py
@@ -0,0 +1,11 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+a, b = [int(i) for i in input().split()]
+
+j = a - b
+l = a+b
+print(str(j)+str(l))
